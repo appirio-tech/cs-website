bb69b0bb6aa2873e512532ff419598e1158d3309
Jeff Douglas <jeff@jeffdouglas.com>
2011-11-01 13:45:18 -0400

    layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,35 @@ module ApplicationHelper
     return cats[0..cats.length-3]
   end
   
+  def format_close_date_time(end_date)
+    
+    end_time = Time.parse(end_date)
+    if end_time.past?
+        display = "Completed"
+    else
+      secs = Time.parse(end_date) - Time.now
+      display = "due in "
+      display += pluralize((secs/86400).floor, 'day')
+      secs = secs%86400
+      display += " " + pluralize((secs/3600).floor, 'hour') + " " + pluralize(((secs%3600)/60).round, 'minute')
+    end
+
+  end
+  
+  def format_close_date(end_date)
+    
+    end_time = Time.parse(end_date)
+    if end_time.past?
+        display = "closed"
+    else
+        secs = Time.parse(end_date) - Time.now
+        if (secs > 86400)
+            display = "due in " + pluralize((secs/86400).floor, 'day')
+        else
+            display = "due in " + pluralize((secs/3600).floor, 'hour') + "  " + pluralize((secs/60).round, 'minute')
+        end
+    end
+
+  end
+  
 end
--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,36 +1,4 @@
-module ChallengesHelper
-  
-  def format_close_date(end_date)
-    
-    end_time = Time.parse(end_date)
-    if end_time.past?
-        display = "closed"
-    else
-        secs = Time.parse(end_date) - Time.now
-        if (secs > 86400)
-            display = "due in " + pluralize((secs/86400).floor, 'day')
-        else
-            display = "due in " + pluralize((secs/3600).floor, 'hour') + "  " + pluralize((secs/60).round, 'minute')
-        end
-    end
-
-  end
-  
-  def format_close_date_time(end_date)
-    
-    end_time = Time.parse(end_date)
-    if end_time.past?
-        display = "Completed"
-    else
-      secs = Time.parse(end_date) - Time.now
-      display = "due in "
-      display += pluralize((secs/86400).floor, 'day')
-      secs = secs%86400
-      display += " " + pluralize((secs/3600).floor, 'hour') + " " + pluralize(((secs%3600)/60).round, 'minute')
-    end
-
-  end
-  
+module ChallengesHelper  
 end
 
 
