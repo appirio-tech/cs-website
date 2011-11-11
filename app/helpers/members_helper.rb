23f537096441fa43f066a3601e5f44dc8c82a3da
Jeff Douglas <jeff@jeffdouglas.com>
2011-11-11 12:20:24 -0500

    tweaking the styles

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,11 @@
 module MembersHelper
+  
+  def member_category_display(categories)
+    cats = ''
+    categories["records"].each do |record|
+      cats = cats + record["Category__r"]["Name"] + ", "
+    end
+    return cats[0..cats.length-3]
+  end
+  
 end
