a989446ca7d26950be344ccc2f9d81c2c2718802
Jeff Douglas <jeff@jeffdouglas.com>
2011-11-17 14:14:44 -0500

    changed omniauth stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['github_oauth_key'], ENV['github_oauth_secret']             # working
   provider :facebook, ENV['facebook_oauth_key'], ENV['facebook_oauth_secret']       # working -- but not on localhost
   provider :linked_in, ENV['linkedin_oauth_key'], ENV['linkedin_oauth_secret']      # have not tried yet
-  provider :salesforce, ENV['sfdc_oauth_key'], ENV['sfdc_oauth_secret']             # does not work
+  provider :salesforce, ENV['sfdc_oauth_key'], ENV['sfdc_oauth_secret']             # does not work -- needs to use a sandbox url in dev & 
   provider :google, "cs-website-sandbox.herokuapp.com", "5WKsvl3O2wzpQtG3k-XnhbqR"  # does not work -- wierd security error
   provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id' # have not tried yet
 end
