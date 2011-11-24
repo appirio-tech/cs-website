3037743596f8bedec10f674fb58bcf11d95fdf15
Jakub Arnold <darthdeus@gmail.com>
2011-11-24 12:23:12 +0100

    fixed #254645 - menu now highlights the current sort/order item, if none is selected, it uses default

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,4 +1,30 @@
 module ChallengesHelper  
+  
+  # 
+  # name - param name
+  # value - param value
+  # text  - link text to be displayed in the menu
+  # other_params - additonal params to be perserved, such as other sorting
+  # opclass - optional class to be added to the link
+  # 
+  def current_link(name, value, text, other_params, opts = {})
+    cls = ""
+    if params[name] == value 
+      cls = "active"
+    # we also want to highlight if it's the default and the parameter isn't set
+    elsif opts[:default] == true && !params.has_key?(name)
+      cls = "active"
+    end
+    cls += " #{opts[:class]}" if opts.has_key?(:class)
+    
+    # merge with any additonal sorting that was already applied
+    params_hash = {}
+    [*other_params].each do |p|
+      params_hash[p] = params[p] 
+    end
+    final_params = params_hash.merge(name => value)    
+    link_to text, challenges_path(final_params), :class => cls
+  end
 end
 
 
