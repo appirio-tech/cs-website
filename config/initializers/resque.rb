d40997b77006b495236baa76667c11158cbe416b
Jeff Douglas <jeff@jeffdouglas.com>
2011-11-28 07:03:38 -0500

    final touchups for resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,8 +7,3 @@ uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
-
-=begin
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
-=end
