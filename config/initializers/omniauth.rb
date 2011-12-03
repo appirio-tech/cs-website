7f893c5bd0557222daa1dcaa334b548925f10db9
Michael Bleigh <michael@intridea.com>
2011-12-03 15:00:13 -0500

    Use sandbox domain if ENV[SANDBOX] is present.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,15 @@
 OmniAuth.config.full_host = ENV['OMNIAUTH_FULL_HOST'] 
 
+sfdc_setup = lambda do |env|
+  env['omniauth.auth'].options[:client_options][:site] = 'https://test.salesforce.com' if ENV['SANDBOX']
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem'
   provider :twitter, ENV['TWITTER_OAUTH_KEY'], ENV['TWITTER_OAUTH_SECRET']
   provider :github, ENV['GITHUB_OAUTH_KEY'], ENV['GITHUB_OAUTH_SECRET']
   provider :facebook, ENV['FACEBOOK_OAUTH_KEY'], ENV['FACEBOOK_OAUTH_SECRET']
   provider :linked_in, ENV['LINKEDIN_OAUTH_KEY'], ENV['LINKEDIN_OAUTH_SECRET']
-  provider :salesforce, ENV['SFDC_OAUTH_KEY'], ENV['SFDC_OAUTH_SECRET']
+  provider :salesforce, ENV['SFDC_OAUTH_KEY'], ENV['SFDC_OAUTH_SECRET'], :setup => sfdc_setup
   provider :openid, :store => OpenID::Store::ActiveRecord.new
 end
