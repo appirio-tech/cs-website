1248aa3a27e0f00b3fd1bcaf2d3443de5df3e81f
Michael Bleigh <michael@intridea.com>
2011-12-03 16:07:43 -0500

    Don't need to handle different providers differently.

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,27 +1,14 @@
 class AuthSession
     def initialize (authhash, provider)
-        @authhash = Hash.new
-        # filling up our uniform hash based on the provider
-        if provider == 'facebook'
-            authhash['extra']['user_hash']['email'] ? @authhash[:email] =  authhash['extra']['user_hash']['email'] : @authhash[:email] = ''
-            authhash['extra']['user_hash']['name'] ? @authhash[:name] =  authhash['extra']['user_hash']['name'] : @authhash[:name] = ''
-            authhash['extra']['user_hash']['id'] ? @authhash[:uid] =  authhash['extra']['user_hash']['id'].to_s : @authhash[:uid] = ''
-            authhash['extra']['user_hash']['username'] ? @authhash[:username] =  authhash['extra']['user_hash']['username'] : @authhash[:username] = ''
-            authhash['provider'] ? @authhash[:provider] = authhash['provider'] : @authhash[:provider] = ''
-        elsif provider == 'github'
-            authhash['info']['email'] ? @authhash[:email] =  authhash['info']['email'] : @authhash[:email] = ''
-            authhash['info']['name'] ? @authhash[:name] =  authhash['info']['name'] : @authhash[:name] = ''
-            authhash['uid'] ? @authhash[:uid] =  authhash['uid'].to_s : @authhash[:uid] = ''
-            authhash['provider'] ? @authhash[:provider] =  authhash['provider'] : @authhash[:provider] = '' 
-            authhash['info']['nickname'] ? @authhash[:username] =  authhash['info']['nickname'] : @authhash[:username] = '' 
-        elsif ['google','twitter','linked_in','salesforce'].index(provider) != nil
-            authhash['info']['email'] ? @authhash[:email] =  authhash['info']['email'] : @authhash[:email] = ''
-            authhash['info']['name'] ? @authhash[:name] =  authhash['info']['name'] : @authhash[:name] = ''
-            authhash['info']['nickname'] ? @authhash[:username] =  authhash['info']['nickname'] : @authhash[:username] = ''
-            authhash['uid'] ? @authhash[:uid] = authhash['uid'].to_s : @authhash[:uid] = ''
-            authhash['provider'] ? @authhash[:provider] = authhash['provider'] : @authhash[:provider] = ''
-        end
-    end 
+      @authhash = Hash.new
+      # filling up our uniform hash based on the provider
+      authhash['info']['email'] ? @authhash[:email] =  authhash['info']['email'] : @authhash[:email] = ''
+      authhash['info']['name'] ? @authhash[:name] =  authhash['info']['name'] : @authhash[:name] = ''
+      authhash['info']['nickname'] ? @authhash[:username] =  authhash['info']['nickname'] : @authhash[:username] = ''
+      authhash['uid'] ? @authhash[:uid] = authhash['uid'].to_s : @authhash[:uid] = ''
+      authhash['provider'] ? @authhash[:provider] = authhash['provider'] : @authhash[:provider] = ''
+    end
+
     def get_hash
         @authhash
     end
