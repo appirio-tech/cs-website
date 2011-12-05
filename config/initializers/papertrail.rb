930c79b5ddbac62e774080791200b4f8f8f00e86
Jeff Douglas <jeff@jeffdouglas.com>
2011-12-05 07:16:12 -0800

    unocmmented mbleigh changes to use papertrail

--- config/initializers/papertrail.rb
+++ config/initializers/papertrail.rb
@@ -1,11 +1,11 @@
-# if ENV["SYSLOG_URL"]
-#   require "remote_syslog_logger"
-#   require "uri"
+if ENV["SYSLOG_URL"]
+  require "remote_syslog_logger"
+  require "uri"
 
-#   url = URI.parse(ENV["SYSLOG_URL"])
+  url = URI.parse(ENV["SYSLOG_URL"])
 
-#   logger = RemoteSyslogLogger.new(url.host, url.port, :program => url.path[1..-1])
-#   logger.level = Logger::INFO
+  logger = RemoteSyslogLogger.new(url.host, url.port, :program => url.path[1..-1])
+  logger.level = Logger::INFO
 
-#   Rails.logger = Rails.application.config.logger = ActionController::Base.logger = Rails.cache.logger = logger
-# end
+  Rails.logger = Rails.application.config.logger = ActionController::Base.logger = Rails.cache.logger = logger
+end
