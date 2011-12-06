34e123080cd2c598f1b728f3ab7aed5875d8042e
Jeff Douglas <jeff@jeffdouglas.com>
2011-12-06 02:45:06 -0800

    added the profile pic to new comments mailer

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -6,10 +6,11 @@ class MemberMailer < ActionMailer::Base
     mail(:subject => "Thank you for registering #{username}")
   end
   
-  def new_challenge_comment(id, name, username, comments)
+  def new_challenge_comment(id, name, username, profile_pic, comments)
     @challenge_name = name
     @challenge_id = id
     @username = username
+    @profile_pic = profile_pic
     @comments = comments
     mail(:subject => "New Comment for '#{name}' Challenge")
   end  
