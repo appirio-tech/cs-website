798389b1a6ebff836048d7567d57deb8a65f9133
Jakub Arnold <darthdeus@gmail.com>
2011-12-14 15:03:49 +0100

    added basic spec for accounts controller

---
+++ spec/controllers/accounts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe AccountsController do
+
+  describe "GET 'index'" do
+    it "should redirect to challenges" do
+      controller.stub!(:require_login).and_return(true)
+      controller.stub!(:get_account).and_return(true)
+      get 'index'
+      response.should redirect_to '/account/challenges'
+    end
+  end
+
+end
