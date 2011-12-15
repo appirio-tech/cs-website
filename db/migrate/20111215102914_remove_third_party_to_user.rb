7d917a84cd2453eeed646164454b267e3566b162
Jeff Douglas <jeff@jeffdouglas.com>
2011-12-15 05:45:47 -0500

    fixed typo

--- db/migrate/20111215102914_remove_third_party_to_user.rb
+++ db/migrate/20111215102914_remove_third_party_to_user.rb
@@ -1,11 +1,11 @@
 class RemoveThirdPartyToUser < ActiveRecord::Migration
   def up
-    remove_column :Users, :third_party_service
-    remove_column :Users, :third_party_service
+    remove_column :users, :third_party_service
+    remove_column :users, :third_party_username
   end
 
   def down
-    add_column :Users, :third_party_service, :string
-    add_column :Users, :third_party_service, :string
+    add_column :users, :third_party_service, :string
+    add_column :users, :third_party_username, :string
   end
 end
