9c7c13155d2b1a03badeee0dbe2faffc6a3313e7
Jeff Douglas <jeff@jeffdouglas.com>
2012-01-04 10:27:37 -0500

    removed the column as only existed in dev

--- db/migrate/20120104144026_remove_third_party_username_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveThirdPartyUsernameFromUser < ActiveRecord::Migration
-  def up
-    remove_column :user, :third_party_username
-  end
-
-  def down
-    add_column :user, :third_party_username, :string 
-  end
-end
