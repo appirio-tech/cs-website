692c8578080a19924c69cc60c74e6fcdb866cc59
Jeff Douglas <jeff@jeffdouglas.com>
2012-01-04 10:20:19 -0500

    changed table name to singular

--- db/migrate/20120104144026_remove_third_party_username_from_user.rb
+++ db/migrate/20120104144026_remove_third_party_username_from_user.rb
@@ -1,9 +1,9 @@
 class RemoveThirdPartyUsernameFromUser < ActiveRecord::Migration
   def up
-    remove_column :users, :third_party_username
+    remove_column :user, :third_party_username
   end
 
   def down
-    add_column :users, :third_party_username, :string 
+    add_column :user, :third_party_username, :string 
   end
 end
