d31d6fd414e594f05612243145ffc5b3acc8c8f9
Jeff Douglas <jeff@jeffdouglas.com>
2012-01-26 17:57:56 -0500

    switched username form auth session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -103,7 +103,7 @@ class SessionsController < ApplicationController
       # user already exists. log them in
       else
 
-        if Services.activate_user(current_access_token, as.get_hash[:username])
+        if Services.activate_user(current_access_token, thirdparty_username(as.get_hash))
         
           # log them in
           user = User.authenticate_third_party(current_access_token, as.get_hash[:provider],thirdparty_username(as.get_hash))
