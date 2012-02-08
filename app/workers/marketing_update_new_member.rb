cca119b116eed3e8a6572e607fed64a793ec54a1
Jeff Douglas <jeff@jeffdouglas.com>
2012-02-08 15:41:35 -0500

    added in queue worker

--- app/workers/marketing_update_new_member.rb
+++ app/workers/marketing_update_new_member.rb
@@ -4,11 +4,15 @@ class WelcomeEmailSender
   @queue = :marketing_update_queue
   def self.perform(access_token, username, marketing_hash)
     
+    Rails.logger.info "[Resque]==== updating new user #{username} with: #{marketing_hash}"
+    
     update_hash = { 'Campaign_Source__c' =>  marketing_hash[:campaign_source], 
       'Campaign_Medium__c' =>  marketing_hash[:campaign_medium], 
       'Campaign_Name__c' =>  marketing_hash[:campaign_name] }
     
-    Members.update(current_access_token, username, update_hash)
+    results = Members.update(current_access_token, username, update_hash)
+    
+    Rails.logger.info "[Resque]==== results from updating marketing info for #{username}: #{results}"
         
   end
   
