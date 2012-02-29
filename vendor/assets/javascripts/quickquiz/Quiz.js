656dedff373da708203ba11f01ffaea31d517a3d
Jeff Douglas <jeff@jeffdouglas.com>
2012-02-29 13:12:15 -0800

    changed url for quiz submission

--- vendor/assets/javascripts/quickquiz/Quiz.js
+++ vendor/assets/javascripts/quickquiz/Quiz.js
@@ -52,7 +52,7 @@ function Quiz(data, components, url){
 		if (self.records[self.curr()].Id != 0) {
 			$.ajax({
 			  type: 'POST',
-			  url: url+'/challenges/quickquiz_answer',
+			  url: url+'/quizes/answer',
 			  data: dataString
 			});
 		}
@@ -86,7 +86,7 @@ function Quiz(data, components, url){
 		if (q.id != 0) {
 			$.ajax({
 			  type: 'POST',
-			  url: url+'/quizes/answer', 
+			  url: url+'/quizes/answer',
 			  data: dataString
 			});
 		}
