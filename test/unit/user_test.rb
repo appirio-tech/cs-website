0b9fa89b8bb19b27d83e9598fb7a5139a4fad980
Jeff Douglas <jeff@jeffdouglas.com>
2012-05-26 08:21:09 -0400

    initial commit

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save" do
+    user = User.new
+    assert !user.save, "Saved the user without all required fields"
+  end
+  
+  test "should save" do
+    user = User.new
+    user.username = 'me'
+    user.password = 'mypass'
+    user.sfdc_username = 'me@something.com'
+    assert user.save, "Could not save the user"
+  end
+  
+  test "should not save without @ in sfdc_username" do
+    #user = users(:one)
+    user = User.new
+    user.username = 'me'
+    user.password = 'mypass'
+    user.sfdc_username = 'mesomething.com'
+    assert !user.save, "Saved the user without a correctly formatted sfdc_username"
+  end  
+
 end
