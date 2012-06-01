ffcd728d1fb588234cc1735f68792d214637654b
Jeff Douglas <jeff@jeffdouglas.com>
2012-06-01 10:46:33 -0400

    removed rails cache logger for dalli

--- config/initializers/papertrail.rb
+++ config/initializers/papertrail.rb
@@ -1,4 +1,3 @@
-=begin
 if ENV["SYSLOG_URL"]
   require "remote_syslog_logger"
   require "uri"
@@ -8,6 +7,5 @@ if ENV["SYSLOG_URL"]
   logger = RemoteSyslogLogger.new(url.host, url.port, :program => url.path[1..-1])
   logger.level = Logger::INFO
 
-  Rails.logger = Rails.application.config.logger = ActionController::Base.logger = Rails.cache.logger = logger
+  Rails.logger = Rails.application.config.logger = ActionController::Base.logger = logger
 end
-=end
