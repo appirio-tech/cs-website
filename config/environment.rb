984f4032a231e2bcd381e697617393b7bfd50b06
Jeff Douglas <jeff@jeffdouglas.com>
2012-06-28 06:12:19 -0400

    added fastpass

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+# load fastpass for getsatisfaction
+require 'fastpass'
 
 # Initialize the rails application
 CloudSpokes::Application.initialize!
