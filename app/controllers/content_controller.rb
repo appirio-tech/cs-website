97c8e942cfbbc93d1173a8d42df78f59c1847d34
Jeff Douglas <jeff@jeffdouglas.com>
2012-09-12 12:14:52 -0500

    switched stats to web service

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -7,25 +7,31 @@ class ContentController < ApplicationController
   
   def home
     @page_title = "A unique cloud development community, focused on mobile technologies and public cloud platforms."
-    @page = Webpages.all(current_access_token, :select => 'id,html__c', :where => 'home')
 
-    @featured_member_username = @page['featured_member_username']
-    @featured_member_pic = @page['featured_member_pic']
-    @featured_member_money = @page['featured_member_money']
-    @featured_member_active = @page['featured_member_active']
-    @featured_member_wins = @page['featured_member_wins']
+    client = Savon.client('http://cs-production.s3.amazonaws.com/cloudspokes-stats-wsdl.xml')
+    response = client.request(:stat, :platform_stats) do
+      soap.namespaces["xmlns:stat"] = "http://soap.sforce.com/schemas/class/StatsWS"
+      soap.header = { 'stat:SessionHeader' => { 'stat:sessionId' => current_access_token }}
+    end 
+    @page = response.to_array(:platform_stats_response, :result).first
+puts "====== #{@page}"
+    @featured_member_username = @page[:featured_member_username]
+    @featured_member_pic = @page[:featured_member_pic]
+    @featured_member_money = @page[:featured_member_money]
+    @featured_member_active = @page[:featured_member_active]
+    @featured_member_wins = @page[:featured_member_wins]
     
-    @members = @page['members']
-    @challenes_open = @page['challenes_open']
-    @chalenges_won = @page['chalenges_won']
-    @money_up_for_grabs = @page['money_up_for_grabs']
-    @money_pending = @page['money_pending']
-    @entries_submitted = @page['entries_submitted']
+    @members = @page[:members]
+    @challenes_open = @page[:challenes_open]
+    @chalenges_won = @page[:chalenges_won]
+    @money_up_for_grabs = @page[:money_up_for_grabs]
+    @money_pending = @page[:money_pending]
+    @entries_submitted = @page[:entries_submitted]
     
-    @featured_challenge_id = @page['featured_challenge_id']
-    @featured_challenge_name = @page['featured_challenge_name']
-    @featured_challenge_prize = @page['featured_challenge_prize']
-    @featured_challenge_details = @page['featured_challenge_details']
+    @featured_challenge_id = @page[:featured_challenge_id]
+    @featured_challenge_name = @page[:featured_challenge_name]
+    @featured_challenge_prize = @page[:featured_challenge_prize]
+    @featured_challenge_details = @page[:featured_challenge_details]
     
     @leaders = Challenges.get_leaderboard(current_access_token, :period => 'all')
     
