04ffdfb3b880df2bdc8addb40729dea8364048fd
Jeff Douglas <jeff@jeffdouglas.com>
2012-09-29 15:06:25 -0400

    removed some debug

--- app/workers/welcome_email_sender.rb
+++ app/workers/welcome_email_sender.rb
@@ -4,14 +4,13 @@ class WelcomeEmailSender
   @queue = :welcome_email_queue
   def self.perform(access_token, membername)
     
-    Rails.logger.info "[Resque]==== sending welcome email for member: #{membername}"
     # fetch the member's email address from sfdc
     member_results = Members.find_by_username(access_token, membername,'Email__c')[0]
-    Rails.logger.info "[Resque]==== sfdc email results for member: #{member_results}"
+    Rails.logger.info "[Resque]==== sfdc email results for member : #{member_results}"
 
     begin
 
-      Rails.logger.info "[Resque]==== sending welcome email to #{member_results['Email__c']}"
+      Rails.logger.info "[Resque]==== sending welcome email to member #{membername} at #{member_results['Email__c']}"
       # generate the mail to send and send it
       mail = MemberMailer.welcome_email(membername, member_results['Email__c']).deliver
 
