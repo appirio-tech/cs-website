4d131fcbfa5b2099dd5fe2b634305f941bef584c
Jeff Douglas <jeff@jeffdouglas.com>
2012-10-10 09:21:17 -0400

    added better output for marketing data to log

--- app/workers/marketing_update_new_member.rb
+++ app/workers/marketing_update_new_member.rb
@@ -4,7 +4,7 @@ class MarketingUpdateNewMember
   @queue = :marketing_update_queue
   def self.perform(access_token, username, marketing_hash)
     
-    Rails.logger.info "[Resque]==== updating new user #{username} with: #{marketing_hash}"
+    Rails.logger.info "[Resque]==== updating new user #{username} with marketing data: #{marketing_hash}"
     
     update_hash = { 'Campaign_Source__c' =>  marketing_hash['campaign_source'], 
       'Campaign_Medium__c' =>  marketing_hash['campaign_medium'], 
