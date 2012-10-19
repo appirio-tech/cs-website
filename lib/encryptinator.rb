5f34ed0b80bad49a26719de1b786cf8bca4149f6
Jeff Douglas <jeff@jeffdouglas.com>
2012-10-19 07:45:03 -0400

    utf-8 encoding

--- lib/encryptinator.rb
+++ lib/encryptinator.rb
@@ -4,13 +4,15 @@ class Encryptinator
 
   def self.encrypt_string(plain_text)
     secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
-    encrypted_value = Encryptor.encrypt(plain_text, :key => secret_key)
+    encrypted_value = Encryptor.encrypt(plain_text, 
+    	:key => secret_key).encode('UTF-8')
     encrypted_value
   end  
 
   def self.decrypt_string(encrypted_text)
     secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
-    decrypted_value = Encryptor.decrypt(encrypted_text, :key => secret_key)
+    decrypted_value = Encryptor.decrypt(encrypted_text, 
+    	:key => secret_key).decode('UTF-8')
     decrypted_value
   end  
 
