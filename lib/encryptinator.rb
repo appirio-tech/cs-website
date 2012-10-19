aed724fade20165f9022ea56d6788e88ab62eb86
Jeff Douglas <jeff@jeffdouglas.com>
2012-10-19 12:07:48 -0400

    encode string

--- lib/encryptinator.rb
+++ lib/encryptinator.rb
@@ -1,17 +1,19 @@
 require 'encryptor'
+require 'base64'
 
 class Encryptinator
 
   def self.encrypt_string(plain_text)
     secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
-    encrypted_value = Encryptor.encrypt(plain_text, 
+    encrypted_value = Encryptor.encrypt(:value => plain_text, 
     	:key => secret_key, :algorithm => 'bf')
-    encrypted_value
+    Base64.encode64(encrypted_value)
   end  
 
   def self.decrypt_string(encrypted_text)
     secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
-    decrypted_value = Encryptor.decrypt(encrypted_text, 
+    decrypted_value = Encryptor.decrypt(
+    	:value => Base64.decode64(encrypted_text), 
     	:key => secret_key, :algorithm => 'bf')
     decrypted_value
   end  
