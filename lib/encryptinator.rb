2732672fa8737e838cbbf8fc5056d0c39f50c2c4
Jeff Douglas <jeff@jeffdouglas.com>
2012-10-19 07:31:58 -0400

    initial commit

---
+++ lib/encryptinator.rb
@@ -0,0 +1,17 @@
+require 'encryptor'
+
+class Encryptinator
+
+  def self.encrypt_string(plain_text)
+    secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
+    encrypted_value = Encryptor.encrypt(plain_text, :key => secret_key)
+    encrypted_value
+  end  
+
+  def self.decrypt_string(encrypted_text)
+    secret_key = Digest::SHA256.hexdigest(ENV['ENCRYPTION_KEY'])
+    decrypted_value = Encryptor.decrypt(encrypted_text, :key => secret_key)
+    decrypted_value
+  end  
+
+end
