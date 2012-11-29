b95efffe097ef7432599bcad8982051ba6c67f17
Jeff Douglas <jeff@jeffdouglas.com>
2012-11-29 06:38:53 -0500

    added in gmail settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,10 +27,17 @@ CloudSpokes::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  # to test the mailer... hard code the username & password
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => "587",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name      => '',
+    :password       => ''
+  }  
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
