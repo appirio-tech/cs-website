47a7b82f5f212b87876e95f5bbfaa686e46c2786
Jeff Douglas <jeff@jeffdouglas.com>
2012-12-18 14:21:19 -0500

    added community signup based upon marketing campaign

--- app/workers/marketing_update_new_member.rb
+++ app/workers/marketing_update_new_member.rb
@@ -1,3 +1,5 @@
+require 'cs_api_community'
+
 class MarketingUpdateNewMember
   include HTTParty 
   
@@ -11,7 +13,14 @@ class MarketingUpdateNewMember
       'Campaign_Name__c' =>  marketing_hash['campaign_name'] }
     
     results = CsApi::Member.update(access_token, membername, update_hash)
-        
+
+    # see if there is a community with a matching campaign name
+    community = SfdcConnection.admin_dbdc_client.query("select id, name, community_id__c from community__c where Marketing_Campaign__c = '#{marketing_hash['campaign_name']}'")
+    # if we found a matching community, add them to it
+    unless community.empty?
+    	CsApi::Community.add_member(access_token, {:membername => membername, :community_id => community.first['Community_Id__c']})
+    end
+
   end
   
 end
