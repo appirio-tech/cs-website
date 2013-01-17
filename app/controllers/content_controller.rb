caef1ac32158d72e69d1207f1a4c3c7827f84e2b
Jeff Douglas <jeff@jeffdouglas.com>
2013-01-17 13:06:44 -0500

    tweaks to sso

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -7,7 +7,37 @@ require 'js_connect'
 
 class ContentController < ApplicationController
   before_filter :redirect_to_http
-  
+
+  def forums
+
+    # Start with the signed in user and add in the client_id
+    user = {email: 'jeff+snippets@jeffdouglas.com', name: 'jeffdonthemic-snippets', 
+        photourl: 'http://lh4.ggpht.com/4wzpuaY9Oz1uNSyjinB72Re8V3DMEEyaeaLzJegV_tHyDYZm2nrNq6E_LuICWFs0-r-E70LgxlHca4qKxXKCSaP2zjarwgg',
+        uniqueid: 'jeffdonthemic-snippets', 
+        client_id: ENV['VANILLA_CLIENT_ID']}
+
+    # json encode the user
+    json = ActiveSupport::JSON.encode(user);     
+    # base 64 encode the user
+    signature_string = Base64.encode64(json)
+    # Sign the signature string with your signature and the current timestamp using hmac sha1 (use vanilla secret)
+    signature = OpenSSL::HMAC.digest('sha1', ENV['VANILLA_SECRET'], signature_string + ' ' +  Time.now.to_s)
+    #signature = Digest::MD5.hexdigest(signature_string + ' ' + Time.now);
+    # # build the final sso string
+    @vanilla_sso = "#{signature_string} #{signature} #{Time.now} hmacsha1"
+
+  end
+
+  def forums_authenticate 
+
+    user = {email: 'jeff+snippets@jeffdouglas.com', name: 'jeffdonthemic-snippets', 
+        photourl: 'http://lh4.ggpht.com/4wzpuaY9Oz1uNSyjinB72Re8V3DMEEyaeaLzJegV_tHyDYZm2nrNq6E_LuICWFs0-r-E70LgxlHca4qKxXKCSaP2zjarwgg',
+        uniqueid: 'jeffdonthemic-snippets'}
+
+    render :json => JsConnect::getJsConnectString(user, request, ENV['VANILLA_CLIENT_ID'], ENV['VANILLA_SECRET']) 
+
+  end
+
   def home
     @page_title = "A unique cloud development community, focused on mobile technologies and public cloud platforms."
 
@@ -38,41 +68,6 @@ class ContentController < ApplicationController
       format.json { render :json => @page }
     end
     
-  end
-
-  def forums
-
-    client_id = '1048311983'
-    secret = 'ac9be007d9819ace8d687405b3139a47'
-
-    #create the signed user as json
-    user = {email: 'jdouglas@appirio.com', name: 'jeffdonthemic', 
-        photourl: 'http://lh4.ggpht.com/4wzpuaY9Oz1uNSyjinB72Re8V3DMEEyaeaLzJegV_tHyDYZm2nrNq6E_LuICWFs0-r-E70LgxlHca4qKxXKCSaP2zjarwgg',
-        uniqueid: 'jeffdonthemic', 
-        client_id: client_id}.to_json
-     
-    # base64 encode the user with the time stamp
-    signature_string = "#{Base64.encode64(user)} #{Time.now.to_i}"
-    # sign the signature string
-    signature = CGI.escape("#{OpenSSL::HMAC.digest('sha1', secret, signature_string)}")
-    # build the final sso string
-    @vanilla_sso = "#{signature_string} #{signature} #{Time.now.to_i} hmacsha1"
-
-  end
-
-  def forums_authenticate 
-
-    client_id = '1048311983'
-    secret = 'ac9be007d9819ace8d687405b3139a47'
-
-    user = {'email' => 'jdouglas@appirio.com', 'name' => 'jeffdonthemic', 
-        'photourl' => 'http://lh4.ggpht.com/4wzpuaY9Oz1uNSyjinB72Re8V3DMEEyaeaLzJegV_tHyDYZm2nrNq6E_LuICWFs0-r-E70LgxlHca4qKxXKCSaP2zjarwgg',
-        'uniqueid' => 'jeffdonthemic'}
-
-    callback = JsConnect::getJsConnectString(user, request, client_id, secret)
-
-    #render as json
-    render :json => callback 
-  end
+  end  
 
 end
